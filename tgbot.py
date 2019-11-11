66e507e684ddc64eff19d04d87f32da7c6fd8339
aleb112 <57605302+aleb112@users.noreply.github.com>
2019-11-11 04:20:53 +0300

    Update tgbot.py

--- tgbot.py
+++ tgbot.py
@@ -4,8 +4,12 @@ import telebot
 owm = pyowm.OWM('8b79b20a5b368c91c5e9898490dc0a8a', language = 'ru')
 bot = telebot.TeleBot("877194843:AAGChcYUQ7Ol945JAHxSczaeuZfha0BbfRs")
 
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Пожалуйста, напишите название города, для которого хотите узнать погоду.')
+
 @bot.message_handler(content_types=['text'])
-def send_echo(message):
+def send_text(message):
 	observation = owm.weather_at_place(message.text)
 	w = observation.get_weather()
 	temp = w.get_temperature('celsius')['temp']
@@ -13,4 +17,4 @@ def send_echo(message):
 	answer += 'Температура приблизительно ' + str(temp) + '\n\n'
 
 	bot.send_message(message.chat.id, answer)
-bot.polling(none_stop = True, timeout=100)
+bot.polling(none_stop = True, timeout = 100)
